bdc1302500fbb0ab4f9430e608625839d31ccfff
luqmanhadisudiana <62825537+luqmanhadisudiana@users.noreply.github.com>
2023-04-30 16:43:02 +0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './assets/scss/style.scss'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import LandingPage from './pages/LandingPage';
+
+
 
 function App() {
   return (
     <div className="App">
-      hai dunia!!!m
+      <Router>
+        <Route path="/" component={LandingPage}></Route>
+      </Router>
     </div>
   );
 }
