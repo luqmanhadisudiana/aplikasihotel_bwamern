e51b9f09ad4a2a90ef2f1a292ddfe4b699ba8f28
luqmanhadisudiana <62825537+luqmanhadisudiana@users.noreply.github.com>
2023-05-10 00:56:44 +0700

    Component Hero

--- src/parts/Header.js
+++ src/parts/Header.js
@@ -14,13 +14,13 @@
                 <BrandIcon/>
             <div className="collapse navbar-collapse">
                 <ul className="navbar-nav ml-auto">
-                    <li className={`nav-item${getNavLinkClass("/")}`}>
+                    <li className={`nav-item active${getNavLinkClass("/")}`}>
                         <Button className="nav-link" type="link" href="/">
                             Home
                         </Button>
                     </li>
-                    <li className={`nav-item${getNavLinkClass("/browse-by")}`}>
-                        <Button className="nav-link" type="link" href="/browse-by">
+                    <li className={`nav-item${getNavLinkClass("/browseby")}`}>
+                        <Button className="nav-link" type="link" href="/browseby">
                             Browse By
                         </Button>
                     </li>
